6c0ff03abc43f35627aa5a71f3be0e9687e71f7a
root <linke0602@live.cn>
2020-03-04 22:29:10 +0800

    修正.ssh文件的错误

--- .ssh/initssh.bat
+++ .ssh/initssh.bat
@@ -3,17 +3,17 @@ set Host=%2
 
 echo "%User%@^%Host%"
 
-IF NOT EXIST ./id_rsa (ssh-keygen -b 2048 -V +520w -f ./id_rsa)
-IF NOT EXIST ./id_rsa.pub (ssh-keygen -b 2048 -V +520w -f ./id_rsa)
+IF NOT EXIST .\id_rsa (ssh-keygen -b 2048 -V +520w -f .\id_rsa)
+IF NOT EXIST .\id_rsa.pub (ssh-keygen -b 2048 -V +520w -f .\id_rsa)
 
-IF NOT EXIST ./id_rsa (echo "Have no private key")
-IF NOT EXIST ./id_rsa.pub (echo "Have no public key")
+IF NOT EXIST .\id_rsa (echo "Have no private key")
+IF NOT EXIST .\id_rsa.pub (echo "Have no public key")
 
-IF EXIST ./id_rsa (ssh %User%^@%Host% "rm -Rf .ssh")
-IF EXIST ./id_rsa (ssh %User%^@%Host% "mkdir -p .ssh")
-IF EXIST ./id_rsa (ssh %User%^@%Host% "mkdir -p .ssh")
-IF EXIST ./id_rsa (scp ./id_rsa.pub %User%^@%Host%:~\.ssh\authorized_keys)
-IF EXIST ./id_rsa (ssh %User%^@%Host% "chmod 700 .ssh")
-IF EXIST ./id_rsa (ssh %User%^@%Host% "chmod 600 .ssh/authorized_keys")
+IF EXIST .\id_rsa (ssh %User%^@%Host% "rm -Rf .ssh")
+IF EXIST .\id_rsa (ssh %User%^@%Host% "mkdir -p .ssh")
+IF EXIST .\id_rsa (ssh %User%^@%Host% "mkdir -p .ssh")
+IF EXIST .\id_rsa (scp ./id_rsa.pub %User%^@%Host%:~/.ssh/authorized_keys)
+IF EXIST .\id_rsa (ssh %User%^@%Host% "chmod 700 .ssh")
+IF EXIST .\id_rsa (ssh %User%^@%Host% "chmod 600 .ssh/authorized_keys")
 
 pause
