c0163fd311bd858f6f434bc6cb80d8c5d3650c36
root <398567387@qq.com>
2020-03-04 21:37:47 +0800

    修正ubuntu下没有开启root远程登陆权限的BUG

--- maker_public/__init__.py
+++ maker_public/__init__.py
@@ -176,6 +176,9 @@ def ConfigSshd():
         "\nAuthorizedKeysFile .ssh/authorized_keys", szSshdConf)
     szSshdConf = re.sub("\\n[ \\t]*#[ \\t]*AuthorizedKeysFile.+", \
         "\nAuthorizedKeysFile .ssh/authorized_keys", szSshdConf)
+    #打开root登陆
+    szSshdConf = re.sub("\\n[ \\t]*#[ \\t]*PermitRootLogin[ \\t]+", \
+        "\nPermitRootLogin ", szSshdConf)
     #写入配置文件
     szErr = writeTxtFile("/etc/ssh/sshd_config", szSshdConf)
     if 0 < len(szErr):
